8c8da4b2d34ec131d5f76664c219758e4edfa838
abichal93 <abichaljha33@gmail.com>
2018-07-18 07:31:31 +0530

    My project commited sucessfully

---
+++ shoppingbakend/src/main/java/abi/mca/logic/shoppingbakend/App.java
@@ -0,0 +1,13 @@
+package abi.mca.logic.shoppingbakend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
